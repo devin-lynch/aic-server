8f11229aaac25f375fda7c9c87993f5a9229f173
devin-lynch <devin.l.lynch@gmail.com>
2023-07-06 18:32:40 -0500

    Implement adding imgId to db

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const PORT = 8000;
 app.use(cors());
 app.use(express.json());
+const db = require('./models');
 
 const url = `https://api.artic.edu/api/v1/artworks`;
 
@@ -22,6 +23,29 @@ app.post('/image', async (req, res) => {
     const response = await fetch(`${url}/${req.body.id}`);
     const data = await response.json();
     res.send(JSON.stringify(data.data.image_id));
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.get('/save', async (req, res) => {
+  try {
+    const savedArt = await db.art.findAll();
+    const artImgIds = savedArt.map((art, i) => {
+      return art.dataValues.imageId;
+    });
+    res.send(JSON.stringify(artImgIds));
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.post('/save', async (req, res) => {
+  try {
+    await db.art.create({
+      imageId: req.body.imgId,
+    });
+    res.status(200).send();
   } catch (error) {
     console.log(error);
   }
